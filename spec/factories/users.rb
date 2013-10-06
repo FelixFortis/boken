87a73da58c98d0c89a8e813a4e1889dab7f6e1c5
Adam Wenham <adamwenham64@gmail.com>
2013-10-06 11:09:05 +0100

    added user factory and specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:username) { |n| "buns#{n}" }
+    email { "#{username}@example.com" }
+    password "abc12345"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "user signup" do
+    before :each do
+      @new_user = FactoryGirl.create(:user)
+    end
+
+    it "accepts a new user with valid credentials" do
+      @new_user.should be_valid
+    end
+
+    it "declines new users with an empty username" do
+      @new_user.username = ""
+      @new_user.should_not be_valid
+    end
+
+    it "declines new users with an invalid email" do
+      @new_user.email = "millhouse"
+      @new_user.should_not be_valid
+    end
+
+    it "declines new users with a blank email" do
+      @new_user.email = ""
+      @new_user.should_not be_valid
+    end
+
+    it "declines new users with an empty password" do
+      @new_user.password = ""
+      @new_user.should_not be_valid
+    end
+
+    it "declines new users with an invalid password" do
+      @new_user.password = "nil"
+      @new_user.should_not be_valid
+    end
+  end
 end
