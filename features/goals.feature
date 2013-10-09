e0278c0b17115c7241d0cb31771f47103136f7f7
Adam Wenham <adamwenham64@gmail.com>
2013-10-09 21:05:16 +0100

    finished writing scenario and step defs for viewing a goal with the show action

--- features/goals.feature
+++ features/goals.feature
@@ -18,3 +18,19 @@ Feature: managing the goals list
       | goal 3 | description 3 | true     |
     And the title should be "adventureList | Global goals"
     And the heading should be "Global goals"
+
+  Scenario: View a goal
+    Given the following goals exist:
+      | name   | description   | complete |
+      | goal 1 | description 1 | true     |
+      | goal 2 | description 2 | false    |
+      | goal 3 | description 3 | true     |
+    When I go to the Goals page
+    And I click the "Show" link in the third row
+    Then I should see the following goal:
+      | Name:        | name 3        |
+      | Description: | description 3 |
+      | Complete:    | true          |
+    And the title should be "adventureList"
+    When I click the 'Back' link
+    Then I should be on the goals page
--- features/step_definitions/goals_steps.rb
+++ features/step_definitions/goals_steps.rb
@@ -1,9 +1,11 @@
 Given(/^the following goals exist:$/) do |goals|
   Goal.create!(goals.hashes)
 end
+
 When(/^I go to the Goals page$/) do
   visit goals_path
 end
+
 Then(/^I should see the following goals:$/) do |expected_table|
   actual_table = [
       ['Name', 'Description', 'Complete'],
@@ -12,9 +14,32 @@ Then(/^I should see the following goals:$/) do |expected_table|
       ['goal 3', 'description 3', 'true']]
   expected_table.diff!(actual_table)
 end
+
 Then(/^the title should be "([^"]*)"$/) do |page_name|
   page.should have_title page_name
 end
+
 Then(/^the heading should be "([^"]*)"$/) do |page_name|
   find('h1').should have_content page_name
+end
+
+When(/^I click the "([^"]*)" link in the third row$/) do |arg|
+  click_link('Show')[2]
+end
+
+Then(/^I should see the following goal:$/) do |expected_table|
+  actual_table = [
+      ['Name', 'name 3'],
+      ['Description', 'description 3'],
+      ['Complete', 'true']]
+  expected_table.diff!(actual_table)
+end
+
+When(/^I click the 'Back' link$/) do
+  click_link 'Back'
+end
+
+Then(/^I should be on the goals page$/) do
+  page.should have_title 'adventureList | Gobal goals'
+  find('h1').should have_content 'Global goals'
 end
